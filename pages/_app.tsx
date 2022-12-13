5e3192677d4ff1bad907ce76c7f303e4cda3dc22
AbelTeha <abelteha@gmail.com>
2022-12-13 15:12:24 +0300

    added the loading bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,25 @@
 import "../styles/globals.css";
+import "../styles/nprogress.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
 import { store } from "../redux/store";
 import { Provider } from "react-redux";
+import NProgress from "nprogress";
+import Router from "next/router";
+import { Fragment } from "react";
+import Head from "next/head";
+import NextNProgress from "nextjs-progressbar";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Provider store={store}>
-        <Component {...pageProps} />
-      </Provider>
-    </Layout>
+    <>
+      <NextNProgress />
+
+      <Layout>
+        <Provider store={store}>
+          <Component {...pageProps} />
+        </Provider>
+      </Layout>
+    </>
   );
 }
